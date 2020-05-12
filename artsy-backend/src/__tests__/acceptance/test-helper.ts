5f597c126dc9c2e91f231180837f08d4f94dd8b4
Martin <martin_hammer@live.dk>
2020-05-12 16:11:19 +0200

    First commit

---
+++ artsy-backend/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,32 @@
+import {ArtsyApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new ArtsyApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: ArtsyApplication;
+  client: Client;
+}
