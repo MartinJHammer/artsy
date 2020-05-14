e97987fe3ef8e4bbdf736b1638e55b4d619d522f
Martin Jul Hammer <martin_hammer@live.dk>
2020-05-14 12:32:03 +0200

    Added mongodb datasource.

---
+++ artsy-backend/src/datasources/database.datasource.ts
@@ -0,0 +1,32 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'database',
+  connector: 'mongodb',
+  url: 'mongodb://localhost:27020/artsy',
+  host: 'localhost',
+  port: 27020,
+  user: '',
+  password: '',
+  database: 'artsy',
+  useNewUrlParser: true
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class DatabaseDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'database';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.database', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
